99f4867ed3510771e670a3247f9f602b9ca78153
Mathias Cassonnet <mathias.cassonnet@epitech.eu>
2021-02-26 11:54:42 +0100

    [MOD] change database with real responses, change action popup

--- app/src/main/java/com/universitedebordeaux/joue_maths_gie/ui/ActionPopupActivity.java
+++ app/src/main/java/com/universitedebordeaux/joue_maths_gie/ui/ActionPopupActivity.java
@@ -1,61 +1,35 @@
 package com.universitedebordeaux.joue_maths_gie.ui;
 
 import android.app.Activity;
-import android.app.Dialog;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import com.universitedebordeaux.joue_maths_gie.R;
-
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.ref.WeakReference;
-
-public class ActionPopupActivity {
+public class ActionPopupActivity extends AbstractPopup {
 
     public interface ActionPopupButtonHandler {
         void onClick(@NotNull String editTextResult);
     }
 
     private EditText etField;
-
-    private final WeakReference<Activity> activity;
     private final ActionPopupButtonHandler buttonHandler;
-    private Dialog dialog;
 
     public ActionPopupActivity(String title, String hintText, Activity activity,
                                ActionPopupButtonHandler buttonHandler) {
-
-        this.activity = new WeakReference<>(activity);
+        super(activity, R.layout.action_popup_activity);
         this.buttonHandler = buttonHandler;
-
-        setDialog();
         setData(title, hintText);
     }
 
-    public void showDialog() {
-        dialog.show();
-    }
-
-    public void dismiss() {
-        dialog.dismiss();
-    }
-
-    private void setDialog() {
-        dialog = new Dialog(activity.get());
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(R.layout.popup_activity);
-    }
-
     private void setData(String title, String hintText) {
-        TextView tvTitle = dialog.findViewById(R.id.popup_title);
-        Button button = dialog.findViewById(R.id.popup_ok_bouton);
+        TextView tvTitle = dialog.findViewById(R.id.action_popup_title);
+        Button button = dialog.findViewById(R.id.action_popup_bouton);
 
         tvTitle.setText(title);
-        etField = dialog.findViewById(R.id.popup_text_field);
+        etField = dialog.findViewById(R.id.action_popup_text_field);
         etField.setHint(hintText);
         button.setOnClickListener(this::onClick);
     }
--- app/src/main/java/com/universitedebordeaux/joue_maths_gie/ui/CardActivity.java
+++ app/src/main/java/com/universitedebordeaux/joue_maths_gie/ui/CardActivity.java
@@ -1,6 +1,11 @@
 package com.universitedebordeaux.joue_maths_gie.ui;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,27 +24,66 @@ public class CardActivity extends AppCompatActivity {
         setContentView(R.layout.card_activity);
 
         fillCardsList();
-        displayCard();
+        setData();
     }
 
-    private void fillCardsList()
-    {
+    private void fillCardsList() {
         Bundle bundle = getIntent().getExtras();
 
         cardsWithLines = bundle.getParcelableArrayList(CameraActivity.cardsList);
     }
 
-    private void displayCard()
-    {
+    private void setData() {
         CardWithLines card = cardsWithLines.get(0);
-        TextView tvCode = findViewById(R.id.card_code);
+        TextView tvCode = findViewById(R.id.alert_popup_text);
         TextView tvTitle = findViewById(R.id.card_title);
         TextView tvText = findViewById(R.id.card_content);
+        Button responseButton = findViewById(R.id.response_button);
+        ImageButton cameraButton = findViewById(R.id.camera_button);
+        ImageButton soundButton = findViewById(R.id.sound_button);
+        Button helpButton = findViewById(R.id.card_help_button);
 
         tvCode.setText(card.card.id);
         tvTitle.setText(card.card.title);
         tvText.setText(card.getText());
-        // Image
-        // Sound
+        // TODO: Image
+        responseButton.setText(responseButton.getText() + card.card.number.toString());
+        responseButton.setOnClickListener(this::onResponseClick);
+        cameraButton.setOnClickListener(this::onCameraClick);
+        helpButton.setText(helpButton.getText() + card.card.number.toString());
+        if (card.card.tip == null || card.card.tip.isEmpty()) {
+            helpButton.setBackgroundColor(Color.GRAY);
+            helpButton.setEnabled(false);
+        } else {
+            helpButton.setOnClickListener(this::onHelpClick);
+        }
+        soundButton.setOnClickListener(this::onSoundClick);
+    }
+
+    private void onResponseClick(View view) {
+        CardWithLines card = cardsWithLines.get(0);
+        AlertPopupActivity alertPopupActivity = new AlertPopupActivity(getString(R.string.response),
+                card.card.answer, this);
+
+        alertPopupActivity.showDialog();
+    }
+
+    private void onCameraClick(View view) {
+        Intent intent = new Intent(this, CameraActivity.class);
+
+        finish();
+        startActivity(intent);
+    }
+
+    private void onSoundClick(View view) {
+        // TODO: Sound
+    }
+
+    private void onHelpClick(View view) {
+        CardWithLines card = cardsWithLines.get(0);
+        AlertPopupActivity alertPopupActivity = new AlertPopupActivity(getString(R.string.help),
+                card.card.tip, this);
+
+        alertPopupActivity.showDialog();
     }
 }
