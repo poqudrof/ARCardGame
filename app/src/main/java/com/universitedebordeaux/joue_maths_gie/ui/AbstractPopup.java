b6219162ea6f5fe837d14aa9d24af41b8aa3d5c1
Mathias Cassonnet <mathias.cassonnet@epitech.eu>
2021-02-26 11:55:28 +0100

    [MOD] add 2 popup for help and reponse button in card activity

---
+++ app/src/main/java/com/universitedebordeaux/joue_maths_gie/ui/AbstractPopup.java
@@ -0,0 +1,33 @@
+package com.universitedebordeaux.joue_maths_gie.ui;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.view.Window;
+import androidx.annotation.LayoutRes;
+
+import java.lang.ref.WeakReference;
+
+public abstract class AbstractPopup {
+
+    protected final WeakReference<Activity> activity;
+    protected Dialog dialog;
+
+    public AbstractPopup(Activity activity, @LayoutRes int layout) {
+        this.activity = new WeakReference<>(activity);
+        setDialog(layout);
+    }
+
+    public void showDialog() {
+        dialog.show();
+    }
+
+    public void dismiss() {
+        dialog.dismiss();
+    }
+
+    private void setDialog(@LayoutRes int layout) {
+        dialog = new Dialog(activity.get());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(layout);
+    }
+}
