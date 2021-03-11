94d581c751031ec92e0b87d0ca87d734e4213e7a
Arthur Lemeux <arthur.lemeux@epitech.eu>
2021-03-11 10:59:00 +0100

    [MOD] Removed unused import in CameraActivity.

--- app/src/main/java/com/universitedebordeaux/joue_maths_gie/ui/CameraActivity.java
+++ app/src/main/java/com/universitedebordeaux/joue_maths_gie/ui/CameraActivity.java
@@ -10,7 +10,6 @@ import android.util.Log;
 import android.view.Display;
 import android.view.SurfaceView;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
